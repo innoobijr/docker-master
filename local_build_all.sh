adf5c56d7a0662a4c5e74c6a3361d509aa69b004
innoobijr <innocentobi@nodenigeria.com>
2020-12-24 00:25:37 +0000

    change the network name to  so its in line with the local config files. Had some issues due to misnaming.

--- local_build_all.sh
+++ local_build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker network create --attachable -d macvlan  --subnet=10.2.0.0/16  --gateway=10.2.0.1  -o parent=$1 inethihome-bridge
+docker network create --attachable -d macvlan  --subnet=10.2.0.0/16  --gateway=10.2.0.1  -o parent=$1 inethi-bridge
 
 cd ./mysql
 ./local_build.sh
